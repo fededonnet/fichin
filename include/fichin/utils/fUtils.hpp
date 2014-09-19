cdf54446d7404735a5c0ba698651b3ab109c48a9
fededonnet <fede.donnet@gmail.com>
2014-09-18 22:08:05 -0300

    remove ';' in inline function body in fUtils.hpp to works in library

--- include/fichin/utils/fUtils.hpp
+++ include/fichin/utils/fUtils.hpp
@@ -8,15 +8,6 @@ using namespace std;
 
 namespace utils {
 
-template<class T>
-inline std::string to_string(T i) {
-	std::stringstream ss;
-	std::string s;
-	ss << i;
-	s = ss.str();	
-	return s;
-}
-
 //---*Retorna la magnitud del sf::Vector2<T>:
 template<class T>
 float length(sf::Vector2<T> const& vector2) {
@@ -35,6 +26,6 @@ sf::Vector2<T> normalize(sf::Vector2<T> const& vector2) {
 	return vector;
 }
 
-};
+}
 
 #endif
